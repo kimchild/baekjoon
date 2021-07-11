658d55dd26b80c4afad9ba278d1fa657d443d96b
kimchild <allcreation@nate.com>
2021-07-11 15:22:41 +0900

    feat(DividedTest.java): 나누기 기능 추가
    백준 알고리즘 나누기 기능 추가
    
    나누기 기능

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		List<String> multiple = getMultipleArray(scanner.nextLine());
-		int answer = getMultipleNumber(multiple);
+		List<String> divided = getDividedArray(scanner.nextLine());
+		double answer = getDividedNumber(divided);
 		System.out.println(answer);
 		System.exit(0);
 	}
 
-	private static int getMultipleNumber(List<String> multiple) {
-		int result = Integer.parseInt(multiple.get(0));
-		for (String s : multiple.subList(1, multiple.size())) {
-			result *= Integer.parseInt(s);
+	private static double getDividedNumber(List<String> divided) {
+		double result = Integer.parseInt(divided.get(0));
+		for (String s : divided.subList(1, divided.size())) {
+			result /= Double.parseDouble(s);
 		}
 		return result;
 	}
 
-	private static List<String> getMultipleArray(String strMultiple) {
-		return new ArrayList<>(Arrays.asList(strMultiple.split(" ")));
+	private static List<String> getDividedArray(String strDivided) {
+		return new ArrayList<>(Arrays.asList(strDivided.split(" ")));
 	}
 }
