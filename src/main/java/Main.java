97f4e8f40cd84d03e2701fb36fd749e7877f7c2b
kimchild <allcreation@nate.com>
2021-07-11 13:58:04 +0900

    feat: 출력 테스트
    백준 입출력 사칙연산 출력 테스트
    
    Closes Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,6 @@
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
-	public static void main(String[] args) throws IOException {
-		//input
-		Scanner scanner = new Scanner(System.in);
-
-		String[] splitScanner = scanner.nextLine().split(" ");
-		int fixMoney = Integer.parseInt(splitScanner[0]);
-		int createPrice = Integer.parseInt(splitScanner[1]);
-		int salePrice = Integer.parseInt(splitScanner[2]);
-
-		//logic
-		int answer = 0;
-		if(createPrice >= salePrice) {
-			answer = -1;
-		} else {
-			while (fixMoney >= 0) {
-				fixMoney -= (salePrice - createPrice);
-				answer++;
-			}
-		}
+	public static void main(String[] args) {
+		String answer = "hello world!";
 
 		//output
 		System.out.println(answer);
