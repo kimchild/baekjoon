cc6924ec4d616b615cefcba0258b18b9a9252550
kimchild <allcreation@nate.com>
2021-07-11 14:02:09 +0900

    feat(WeLoveKriiiTest.java): WeLoveKriii
    백준 개행 출력문제 WeLoveKriii
    
    WeLoveKriii

---
+++ src/test/java/io/WeLoveKriiiTest.java
@@ -0,0 +1,11 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+
+public class WeLoveKriiiTest {
+	@Test
+	public void HelloWorld() {
+		String answer = "강한친구 대한육군\n강한친구 대한육군";
+		assert "강한친구 대한육군\n강한친구 대한육군".equals(answer);
+	}
+}
