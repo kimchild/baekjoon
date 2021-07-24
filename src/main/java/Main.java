13a95b665478b58ebc0ab7aee87fb498fac44f4b
kimchild <allcreation@nate.com>
2021-07-24 17:37:19 +0900

    feat: 사람이 직접 곱셈식으로 풀때를 출력하는 기능
    사람이 직접 곱셈식으로 풀때를 출력하는 기능
    
    2588	곱셈

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		List<String> numbers = getArray(scanner.nextLine());
+		int target = Integer.parseInt(scanner.nextLine());
+		List<Integer> numbers = getArray(scanner.nextLine());
 
-		int a = Integer.parseInt(numbers.get(0));
-		int b = Integer.parseInt(numbers.get(1));
-		int c = Integer.parseInt(numbers.get(2));
+		int a = numbers.get(0);
+		int b = numbers.get(1);
+		int c = numbers.get(2);
 
-		int answer = (a + b) % c;
+		int answer = target * c;
 		System.out.println(answer);
 
-		answer = ((a % c) + (b % c)) % c;
+		answer = target * b;
 		System.out.println(answer);
 
-		answer = (a * b) % c;
+		answer = target * a;
 		System.out.println(answer);
 
-		answer = ((a % c) * (b % c)) % c;
+		answer = (target * (a * 100)) + (target * (b * 10)) + (target * c);
 		System.out.println(answer);
 
 		System.exit(0);
 	}
 
-	private static List<String> getArray(String strMinus) {
-		return new ArrayList<>(Arrays.asList(strMinus.split(" ")));
+	private static List<Integer> getArray(String strMinus) {
+		List<Integer> list = new LinkedList<>();
+		list.add(Integer.valueOf(strMinus.substring(0, 1)));
+		list.add(Integer.valueOf(strMinus.substring(1, 2)));
+		list.add(Integer.valueOf(strMinus.substring(2, 3)));
+		return list;
 	}
 
 }
